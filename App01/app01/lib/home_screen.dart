fc109f83c7e16237b4eea7c3abd5b56414826aa9
Antonio Mateo <antonio.mateo901222@gmail.com>
2022-10-05 23:21:35 -0500

    inicial

---
+++ App01/app01/lib/home_screen.dart
@@ -0,0 +1,15 @@
+//home_screen.dart <- Snake Case:Estructura de serpiente
+//homeScreen.dart <- Camel Case:Estructura de escalera
+
+import 'package:flutter/material.dart';
+
+class HolaScreen extends StatelessWidget {
+  const HolaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Saludos desde HomeScreen"),
+    );
+  }
+}
